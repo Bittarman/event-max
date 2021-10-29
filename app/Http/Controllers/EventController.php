4c6a86692cc065189517de1b5bfe23df1fb5e08c
Ryan Mauger <ryan.mauger@receipt-bank.com>
2021-10-29 10:33:41 +0100

    Removed unused methods from events controller
    We don't need these methods, as we don't intend to use them

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -42,40 +42,6 @@ class EventController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Event $event)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Event $event)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Event $event)
-    {
-        //
-    }
-
-    /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Event  $event
